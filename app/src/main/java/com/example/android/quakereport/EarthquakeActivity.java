3a413869509629c08a7282572b77582a55a0db10
harshitm98 <harshitmah98@gmail.com>
2017-07-08 19:55:16 +0530

    Error with Adapter
    final WordAdapter adapter = new WordAdapter(this, results); , here 'this', it refers to EarthquakeActivity.EarthquakeAsynTask, thus it we have to make an empty Adapter first and then add the item to the adapter

--- app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
+++ app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
@@ -17,6 +17,7 @@ package com.example.android.quakereport;
 
 import android.content.Intent;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -37,32 +38,48 @@ public class EarthquakeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.earthquake_activity);
 
-        // Create a fake list of earthquake locations.
-        final ArrayList<EarthquakeDetails> earthquakes = QueryUtils.fetchEarthquakeData(requestUrl);
 
 
-        // Find a reference to the {@link ListView} in the layout
-        final ListView earthquakeListView = (ListView) findViewById(R.id.list);
 
-        // Create a new {@link ArrayAdapter} of earthquakes
-        final WordAdapter adapter = new WordAdapter(this, earthquakes);
 
-        earthquakeListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+    }
 
-                EarthquakeDetails item = earthquakes.get(i);
-                Uri webpage = Uri.parse(item.getUrl());
-                Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
-                if (intent.resolveActivity(getPackageManager()) != null) {
-                    startActivity(intent);
+    protected class EarthquakeAsyncTask extends AsyncTask<String, Void, ArrayList<EarthquakeDetails>>{
+
+        @Override
+        protected ArrayList<EarthquakeDetails> doInBackground(String... url) {
+
+            // Create a fake list of earthquake locations.
+            ArrayList<EarthquakeDetails> results = QueryUtils.fetchEarthquakeData(requestUrl);
+
+            return results;
+        }
+
+
+        protected void onPostExecute(final ArrayList<EarthquakeDetails> results) {
+            // Find a reference to the {@link ListView} in the layout
+            final ListView earthquakeListView = (ListView) findViewById(R.id.list);
+
+            // Create a new {@link ArrayAdapter} of earthquakes
+            final WordAdapter adapter = new WordAdapter(this, results);
+
+            earthquakeListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+                    EarthquakeDetails item = results.get(i);
+                    Uri webpage = Uri.parse(item.getUrl());
+                    Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
+                    if (intent.resolveActivity(getPackageManager()) != null) {
+                        startActivity(intent);
+                    }
                 }
-            }
-        });
+            });
 
 
-        // Set the adapter on the {@link ListView}
-        // so the list can be populated in the user interface
-        earthquakeListView.setAdapter(adapter);
+            // Set the adapter on the {@link ListView}
+            // so the list can be populated in the user interface
+            earthquakeListView.setAdapter(adapter);
+        }
     }
 }
